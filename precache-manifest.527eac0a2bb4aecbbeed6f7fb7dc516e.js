b078c8318db6378f952fe95356f49d9a8d0a60de
Adi-2025 <cool.adityaraj2012@gmail.com>
2020-09-25 11:21:53 +0530

    Updates

---
+++ precache-manifest.527eac0a2bb4aecbbeed6f7fb7dc516e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd74a63e2cd72d4a99426fa1aa775f1",
+    "url": "/livescore/index.html"
+  },
+  {
+    "revision": "7aab6a1dd53ab7ef714c",
+    "url": "/livescore/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "2ad2970442dc30d44408",
+    "url": "/livescore/static/css/main.188b2f46.chunk.css"
+  },
+  {
+    "revision": "7aab6a1dd53ab7ef714c",
+    "url": "/livescore/static/js/2.56624c55.chunk.js"
+  },
+  {
+    "revision": "edce152d4e779715f1a35488d0b57adb",
+    "url": "/livescore/static/js/2.56624c55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad2970442dc30d44408",
+    "url": "/livescore/static/js/main.1c3fa233.chunk.js"
+  },
+  {
+    "revision": "93dc1eedc9fd357d163b",
+    "url": "/livescore/static/js/runtime-main.f530306b.js"
+  },
+  {
+    "revision": "7f8a99646772807af879679246659796",
+    "url": "/livescore/static/media/cri2.7f8a9964.jfif"
+  },
+  {
+    "revision": "6e3c4ac45aba4ec4a3bb364c11b9acf4",
+    "url": "/livescore/static/media/cri4.6e3c4ac4.jfif"
+  },
+  {
+    "revision": "9bf191e90aa3928848849406d236da99",
+    "url": "/livescore/static/media/vs.9bf191e9.png"
+  }
+]);
